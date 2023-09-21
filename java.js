f07358056ce441d83ad5368cff46fe642422d1eb
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2023-09-21 16:29:42 -0400

    attempted to make oject

---
+++ java.js
@@ -0,0 +1,12 @@
+function froyOrder () {
+    const input = prompt('Enter your Froyo order seperated by commas:')
+}
+froyOrder();
+
+const userObjectArray = userObjectText.split(',');
+const froyOrder = {
+ orderedFlavors: userObjectArray
+};
+console.log(froyo);
+console.table(froyo);
+
