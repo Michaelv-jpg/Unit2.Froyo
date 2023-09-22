084c5c851569fbec7747dbdd748f85ca411c84af
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2023-09-21 22:50:05 -0400

    finished array

--- java.js
+++ java.js
@@ -1,12 +1,4 @@
-function froyOrder () {
-    const input = prompt('Enter your Froyo order seperated by commas:')
-}
-froyOrder();
-
-const userObjectArray = userObjectText.split(',');
-const froyOrder = {
- orderedFlavors: userObjectArray
-};
-console.log(froyo);
-console.table(froyo);
-
+let input = prompt('Enter your Froyo order seperated by commas:')
+let array = input.split(",");
+console.log(array);
+let froyo = ["chocolate", "vanilla", "coffee", "strawberry" ]
